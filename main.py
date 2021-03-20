34a90732af41371f318af1c7595413a6c51cd8a3
Fedi3 <pawelfedorczak98@gmail.com>
2021-03-21 00:09:27 +0100

    fix print thinking

--- main.py
+++ main.py
@@ -58,9 +58,9 @@ class Philosopher(threading.Thread):
 
     def thinking(self):
         self.status = '0'
+        print(f'Philosopher {self.number}', colored('started', 'yellow'), colored('thinking', 'yellow'))
         lock.release()
         think_time = 2.5
-        # print(f'Philosopher {self.number}',colored('started','yellow'),colored('thinking', 'yellow'))
         time.sleep(think_time)
 
     def checking(self) -> bool:
