bbe08f10f0abbc474db8f7f8172cd5ba9789ee2f
Fedi3 <pawelfedorczak98@gmail.com>
2021-03-20 17:24:27 +0100

    done

--- main.py
+++ main.py
@@ -1,11 +1,116 @@
+import threading
+import time
+lock = threading.Lock()
+N = 5
+TABLE = []
+FORKS = []
+
+#States
+# 0 - Thinking
+# 1 - Getting Hungry
+# 2 - Eating spaghetti
 
 
-def print_hi(name):
+class Philosopher(threading.Thread):
+    def __init__(self, number):
+        super().__init__(target=self.lifecycle)
+        self.number = number
+        self.spaghetti = 0
+        self.status = '0'
+        self.cnd = threading.Condition()
 
-    print(f'Hi, {name}')
+    def lifecycle(self):
+        self.cnd.acquire()
+        while True:
+            self.hungry()
+            #print("cp1")
+            while not self.checking():
+                #print("cp2")
+                self.cnd.wait()
+            self.eating()
+            #print("cp3")
+            self.thinking()
+
+    def eating(self):
+        self.status = '2'
+        eating_time = 2.5
+        #print("cp4")
+        FORKS[self.number].pick_up(self)
+        FORKS[(self.number + 1) % N].pick_up(self)
+        print(f'Philosopher {self.number} started eating')
+        time.sleep(eating_time)
+        self.spaghetti += 1
+        FORKS[self.number].put_back(self)
+        FORKS[(self.number + 1) % N].put_back(self)
+        print(f'Philosopher {self.number} finished eating')
+
+        if TABLE[(self.number + 1) % N].status == "1":
+            TABLE[(self.number + 1) % N].cnd.acquire()
+            TABLE[(self.number + 1) % N].cnd.notify()
+            TABLE[(self.number + 1) % N].cnd.release()
+        if TABLE[(self.number - 1) % N].status == "1":
+            TABLE[(self.number - 1) % N].cnd.acquire()
+            TABLE[(self.number - 1) % N].cnd.notify()
+            TABLE[(self.number - 1) % N].cnd.release()
+
+    def thinking(self):
+        self.status = '0'
+        lock.release()
+        think_time = 2.5
+        time.sleep(think_time)
+
+    def checking(self) -> bool:
+        lock.acquire()
+        if self.status == '1':
+            if FORKS[self.number].taken or FORKS[(self.number + 1) % N].taken:
+                lock.release()
+                return False
+
+        if TABLE[(self.number - 1) % N].status == "1" and TABLE[(self.number - 1) % N].spaghetti < self.spaghetti:
+            TABLE[(self.number - 1) % N].cnd.acquire()
+            TABLE[(self.number - 1) % N].cnd.notify()
+            TABLE[(self.number - 1) % N].cnd.release()
+            lock.release()
+            return False
+
+        if TABLE[(self.number + 1) % N].status == "1" and TABLE[(self.number + 1) % N].spaghetti < self.spaghetti:
+            TABLE[(self.number + 1) % N].cnd.acquire()
+            TABLE[(self.number + 1) % N].cnd.notify()
+            TABLE[(self.number + 1) % N].cnd.release()
+            lock.release()
+            return False
+        return True
+
+    def hungry(self):
+        lock.acquire()
+        self.status = '1'
+        lock.release()
+
+
+class Fork:
+    def __init__(self, number):
+        self.number = number
+        self.taken = False
+
+    def put_back(self, p):
+        print(f'Philosopher {p.number} put back Fork {self.number}\n')
+        self.taken = False
+
+    def pick_up(self, p):
+        print(f'Philosopher {p.number} took Fork {self.number}\n')
+        self.taken = True
+
+
+def main():
+    print('start')
+    global N, TABLE, FORKS
+    TABLE = [Philosopher(i) for i in range(N)]
+    FORKS = [Fork(i) for i in range(N)]
+
+    for i in TABLE:
+        i.start()
 
 
 if __name__ == '__main__':
-
-    print_hi('PyCharm')
+    main()
 
